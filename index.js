49ea0f70e71ab238883ea50758652f6d5aa5c348
Jean M Cetoute <jeanmarylerate51@gmail.com>
2019-09-19 09:33:43 -0500

    added largest number function

--- index.js
+++ index.js
@@ -24,3 +24,14 @@ function findNumber(arr, k) {
 	}
 	return 'NO';
 }
+
+//Find the largest numbers
+const numbers = [ 1, 2, 3, 4, 5, 6 ];
+console.log(Math.max(...numbers));
+
+function largestNumber(num) {
+	for (i = 0; i < num.length; i++) {
+		if (num) {
+		}
+	}
+}
